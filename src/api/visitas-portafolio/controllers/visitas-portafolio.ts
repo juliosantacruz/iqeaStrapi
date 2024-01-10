f941d2937ee3e907a23cabc36bc4169e3adc9e73
Julio Santacruz <julio.santacruzh@gmail.com>
2024-01-10 12:51:01 -0800

    se agrego findOne(slug) a visitas-portafolio

--- src/api/visitas-portafolio/controllers/visitas-portafolio.ts
+++ src/api/visitas-portafolio/controllers/visitas-portafolio.ts
@@ -2,6 +2,20 @@
  * visitas-portafolio controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::visitas-portafolio.visitas-portafolio');
+export default factories.createCoreController(
+  "api::visitas-portafolio.visitas-portafolio",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id } = ctx.params;
+      const entity = await strapi.db
+        .query("api::visitas-portafolio.visitas-portafolio")
+        .findOne({
+          where: { slug: id },
+        });
+      const sanatizedEntity = await this.sanitizeOutput(entity, ctx);
+      return this.transformResponse(sanatizedEntity);
+    },
+  })
+);
